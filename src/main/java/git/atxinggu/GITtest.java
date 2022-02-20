dc7f2acb8ec32a5bd9f71b58d9c6fc15a144a099
lxt <576689799@qq.com>
2022-02-20 10:34:27 +0800

    Commit Second Master

--- src/main/java/git/atxinggu/GITtest.java
+++ src/main/java/git/atxinggu/GITtest.java
@@ -3,5 +3,6 @@ package git.atxinggu;
 public class GITtest {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello git1");
     }
 }
