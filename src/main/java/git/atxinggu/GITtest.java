d444fea3a9bba0a097506c45ab412c4544b45e1a
lxt <576689799@qq.com>
2022-02-20 10:35:41 +0800

    Commit Second Hot-fix

--- src/main/java/git/atxinggu/GITtest.java
+++ src/main/java/git/atxinggu/GITtest.java
@@ -4,5 +4,6 @@ public class GITtest {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello git1");
+        System.out.println("Hello hot-fix");
     }
 }
